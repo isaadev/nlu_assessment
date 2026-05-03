86d38c11887d234d5cc2a5cf3a5f852c9c160c5a
i <101498370+isaadev@users.noreply.github.com>
2026-05-02 23:27:05 -0500

    add data ingestion script and table schema

--- notes.md
+++ notes.md
@@ -6,3 +6,39 @@
 - **violations**: `id`, `address`, `violation_date`, `violation_code`, `violation_status`, `violation_description`, `violation_inspector_comments`
 - **scofflaws**: `id`, `address`
 - **comments**: `id`, `address`, `author`, `datetime`, `comment
+
+
+
+# SQL
+---
+```sql
+	CREATE TABLE violations(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	ADDRESS TEXT,
+	VIOLATION_DATE TEXT,
+	VIOLATION_DESCRIPTION text,
+	VIOLATION_STATUS TEXT,
+	COMMENTS TEXT,
+	VIOLATION_CODE TEXT
+	);
+
+CREATE INDEX idx_violations_address ON violations (address);
+```
+
+```sql
+CREATE TABLE scofflaws(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	ADDRESS TEXT
+);
+```
+
+
+```SQL
+CREATE TABLE COMMENTS(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	ADDRESS TEXT,
+	AUTHOR TEXT,
+	DATETIME TEXT,
+	COMMENT TEXT
+	);
+```
--- scripts/script.py
+++ scripts/script.py
@@ -1,7 +1,53 @@
 import csv
 import sqlite3
+from datetime import datetime, date
 
+cn = sqlite3.connect('test.db')
+ddl = open('scripts/tables.sql').read()
+cn.executescript(ddl)
+cursor = cn.cursor()
 
-cn = sqlite.connect('chicago_db')
+with open('datasets/Building_Code_Scofflaw_List_20250807.csv') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        cursor.execute(
+            """
+            INSERT INTO scofflaws (address)
+            VALUES (?)
+            """,
+            (row["ADDRESS"].strip().upper(),)
+        )
+    cn.commit()
+    
+with open('datasets/Building_Violations_20250815.csv') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        d = datetime.strptime(row["VIOLATION DATE"], "%m/%d/%Y").date()
 
+        if d < date(2024,1,1):
+            continue
+        else:
+            cursor.execute(
+             """
+                INSERT INTO violations(
+                
+                     address,
+                    violation_date,
+                    violation_status,
+                     violation_description,
+                     violation_inspector_comments,
+                    violation_code
+                )
+                VALUES(?,?,?,?,?,?)
+                """,
+                (
+                    row["ADDRESS"],
+                    row["VIOLATION DATE"],
+                    row["VIOLATION STATUS"],
+                    row["VIOLATION DESCRIPTION"],
+                    row["VIOLATION INSPECTOR COMMENTS"],
+                    row["VIOLATION CODE"],
+                )
+            )
+cn.commit()
 
